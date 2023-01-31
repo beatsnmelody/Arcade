ec6c282a0e4ad6bc524474bd3a9530fcf77130b2
Alexandra Labaisse <beatsnmelody1@gmail.com>
2023-01-31 17:10:01 -0600

    finally made the snake move in all four directions

--- index2.js
+++ index2.js
@@ -1,43 +1,81 @@
 let bodyContainer = document.body;
 const snk = document.getElementById("se");
 const board = document.getElementById("board");
+const appl = document.getElementById("apl")
+const start = document.getElementById("start");
+const startover = document.getElementById("startover");
 
 window.addEventListener('load', (e) => {
-    snk.style.left = 0;
-    snk.style.top = 0;
+  snk.style.left = 0;
+  snk.style.top = 0;
 })
 
-let startNumber = 100;
+let startNumber = 8;
 
 
 
-function makeBoard(){
-    for(let i = 0; i <= 15; i++){
-        let createBoard = document.createElement("div")
-        createBoard.classList.add("cell")
-        board.appendChild(createBoard)
-         //console.log("createBoard")
+function makeBoard() {
+  for (let i = 0; i <= 8; i++) {
+    let createBoard = document.createElement("div")
+    createBoard.classList.add("cell")
+    board.appendChild(createBoard)
+    //console.log("createBoard")
 
-    }
-  }   
- 
-  makeBoard()
+  }
+}
+
+makeBoard()
 
 bodyContainer.addEventListener('keydown', (e) => {
 
   switch (e.key) {
     case "Down": // IE/Edge specific value
     case "ArrowDown":
-        console.log(parseInt(snk.style.top))
-        if(parseInt(snk.style.top) < 400){
-            snk.style.top = parseInt(snk.style.top) + startNumber + 'px';
-        } else {
-            console.log('you hit the edge')
-        }
-        
-      break;
-      case "ArrowUp":
-        snk.style.top = parseInt(snk.style.top) - startNumber + 'px';
+      console.log(parseInt(snk.style.top))
+      if (parseInt(snk.style.top) < 256) {
+        snk.style.top = parseInt(snk.style.top) + 8 + 'px';
+      } else {
+        console.log('you hit the edge')
+      }
+
       break;
     }
+    
+  switch (e.key) {
+    case "Up":
+    case "ArrowUp":
+      console.log(parseInt(snk.style.top))
+      if (parseInt(snk.style.top) < 256) {
+        snk.style.top = parseInt(snk.style.top) + -8 + 'px';
+      } else {
+        console.log('you hit the edge')
+      }
+      break;
+    }
+    switch (e.key) {
+      case "Left":
+      case "ArrowLeft":
+      console.log(parseInt(snk.style.left))
+      if (parseInt(snk.style.left) < 256) {
+        snk.style.left = parseInt(snk.style.left) + -8 + 'px';
+      } else {
+        console.log('you hit the edge')
+      }
+      break;
+    }
+    switch (e.key) {
+      case "Right":
+      case "ArrowRight":
+      console.log(parseInt(snk.style.left))
+      if (parseInt(snk.style.left) < 256) {
+        snk.style.left = parseInt(snk.style.left) + 8 + 'px';
+      } else {
+        console.log('you hit the edge')
+      }
+      break;
+  }
 });
+
+function applePowerup() {
+  
+}
